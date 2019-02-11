3820e3dbed9d50d97dc4d7c6954765b175e613ed
sakul <sakul.s2557@gmail.com>
2019-02-11 16:25:42 +0700

    recharts1

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import './App.css';
 import CustomPaginationActionsTable from './components/CustomPaginationActionsTable'
 import SimpleTabs from './components/SimpleTabs'
 import SimpleMap from './components/SimpleMap'
+import { BarChart, XAxis, YAxis, Tooltip, Legend, CartesianGrid, Bar } from 'recharts';
+
 
 class App extends Component {
 
   render() {
-    
-    
+    let data = [{ name: 'a', pv: 12, uv: 15 }]
+
     return (
       <div className="App">
         <SimpleTabs />
+        <BarChart width={730} height={250} data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+        </BarChart>
       </div>
     );
   }

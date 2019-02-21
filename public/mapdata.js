da514946ba92590e012c56c7cf1c23f82897d3a2
sakul <sakul.s2557@gmail.com>
2019-02-22 04:21:55 +0700

    api axios

--- public/mapdata.js
+++ public/mapdata.js
@@ -1,7 +1,7 @@
 var simplemaps_countrymap_mapdata={
   main_settings: {
    //General settings
-    width: "300", //'700' or 'responsive'
+    width: "700", //'700' or 'responsive'
     background_color: "#FFFFFF",
     background_transparent: "yes",
     border_color: "#ffffff",
@@ -537,3 +537,22 @@ var simplemaps_countrymap_mapdata={
     }
   }
 };
+
+// const axios = require('axios')
+
+// const getData = async () => {
+//     try {
+//         return await axios.get('https://script.googleusercontent.com/macros/echo?user_content_key=5kk20HhNfn4w1OdIuacnxU81_x2W2jRazgbxzcQJHgj8_0yUTYfpq_pneGBSfLjMDclae3FzHImlmWaQ9a5FmnrK5FCt8KWFm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPXLUnwHGKzprFvU-TcH3fxYB9jxnvc9LP3_g0mI4sdN4PmjUzIzYmUzuLnHtYUVHUWY2nZ7HPw5&lib=Md1e6MZdc0_HwF7DsIByc-zfIqMXwq4Jm')
+//     }catch(error) {
+//         console.error(error)
+//     }
+// }
+
+// const countData = async () => {
+//   const data = await getData()
+//   var str = data.data 
+//   console.log(str);
+  
+// }
+
+// countData()
